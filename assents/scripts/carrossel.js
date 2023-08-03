f18fa813658255828e4cfea9be09756f53f2c41f
Kaio Luiz Ferreira <53657502+kaio358@users.noreply.github.com>
2023-08-03 17:31:12 -0300

    tenho erros eu sei

--- assents/scripts/carrossel.js
+++ assents/scripts/carrossel.js
@@ -1,29 +1,98 @@
+
+
+let posicao = document.getElementsByClassName("posicao")[0]
+
+//  fetch('db_img.json').then((resp) => resp.json()).then((dados)=>console.log(dados)).catch(err=> console.log(err))
+
+let img = {
+    carros:[
+        {
+                src_img:"./assents/imgs/ford/escape_fechado_frente.png",
+                name:"Ford Escape"
+        },
+        
+    ]
+    
+}
+console.log(img);
+
+let imgs = ["./assents/imgs/ford/escape_fechado_frente.png","./assents/imgs/maserati/conversivel/granCabrioLevante_vermelho.png","./assents/imgs/mercedes/van_sprinter/sprinter_2015.png"]
+
+function createElement(quantidade = 1){
+    for(var i=1; i<= quantidade;i++){
+        var itens = document.createElement('div')
+        itens.classList.add("itens-carrossel")
+        posicao.appendChild(itens)
+    }
+}
+createElement(imgs.length)
+
 let item = document.getElementsByClassName("itens-carrossel")
 var index = 0
-item[index].style. background = "var( --mazarine-blue)"
-item[index].style.padding = "15px"
-function muda(frente =true) {
-    
+var lagura = window.innerWidth
+
+let carro_destaque = document.querySelector(".carro_destaque")
+
+
+// primeira imagem
+if(lagura > 1024){
+    item[index].style. background = "var( --mazarine-blue)"
+    item[index].style.padding = "15px"
+
+}else{
+    item[index].style. background = "var( --mazarine-blue)"
+    item[index].style.padding = "8px"
+}
+
+// largura
+window.addEventListener('resize',()=>{
+   lagura =  window.innerWidth
+})
+
+
+// cria elementos
+
+
+
+// tudo para passar o elemento
+
+function muda() {
     item[index].style. background = "var(--lynx-white)"
-    item[index].style.padding = "10px"
+    
+    if(lagura > 1024){
+        item[index].style.padding = "10px"
+        passa()
+        item[index].style.padding = "15px";
+        carro_destaque.src = imgs[index]
+
+    } else{
+        item[index].style.padding = "5px"
+        passa()
+        item[index].style.padding = "8px"
+       
+        carro_destaque.src = imgs[index]
+    }
+    
+
+}
+
+function passa(frente = true){
     if(frente){
         index++
 
-        if(index>2){
+        if(index>=item.length){
             index = 0
         }
         item[index].style. background = "var( --mazarine-blue)"
-        item[index].style.padding = "15px"
+        
     }else{
         index--
     
         if(index<0){
-            index = 2
+            index = item.length -1
         }
         item[index].style. background = "var( --mazarine-blue)"
-        item[index].style.padding = "15px"
+       
     }
-        
-
 }
 setInterval(muda, 6000)
---
+++ assents/scripts/teste.js
@@ -0,0 +1,10 @@
+function naosei() {
+    return
+    
+}
+
+
+
+// ,"./assents/imgs/maserati/conversivel/granCabrioLevante_vermelho.png","./assents/imgs/mercedes/van_sprinter/sprinter_2015.png"
+
+
